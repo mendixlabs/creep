1a9a285001ac685ba138730f2ccbb70528bdb3da
Achiel van der Mandele <achiel@kretek.local>
2012-05-27 00:03:05 +0200

    cosmetics

--- bot.py
+++ bot.py
@@ -9,38 +9,37 @@ config = ConfigParser()
 config.read(['creep.cfg', '/usr/local/etc/creep.cfg'])
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    jid = config.get('xmpp', 'username')
-    password = config.get('xmpp', 'password')
-    room = config.get('xmpp', 'room')
-    server = config.get('xmpp', 'server')
-    resource = config.get('xmpp', 'resource')
-    host = config.get('http', 'host')
-    port = int(config.get('http', 'port'))
+jid = config.get('xmpp', 'username')
+password = config.get('xmpp', 'password')
+room = config.get('xmpp', 'room')
+server = config.get('xmpp', 'server')
+resource = config.get('xmpp', 'resource')
+host = config.get('http', 'host')
+port = int(config.get('http', 'port'))
 
-    conn = sleekxmpp.ClientXMPP('%s/%s' % (jid, resource), password)
-    conn.register_plugin('xep_0045')
+conn = sleekxmpp.ClientXMPP('%s/%s' % (jid, resource), password)
+conn.register_plugin('xep_0045')
 
-    @app.route("/", methods=['POST',])
-    def index():
-        if request.content_type == 'application/json':
-            msg = request.json['message']
-        else:
-            msg = request.data
-        conn.send_message(mto=room, mbody="%s" % msg, mtype='groupchat')
-        return "message sent"
+@app.route("/", methods=['POST',])
+def index():
+    if request.content_type == 'application/json':
+        msg = request.json['message']
+    else:
+        msg = request.data
+    conn.send_message(mto=room, mbody="%s" % msg, mtype='groupchat')
+    return "message sent\n"
 
-    logging.info("Connecting %s to xmppserver %s" % (jid, server))
-    conn.connect((server, 5222))
-    logging.info("Connected")
+logging.info("Connecting %s to xmppserver %s" % (jid, server))
+conn.connect((server, 5222))
+logging.info("Connected")
 
-    conn.process(threaded=True)
-    def handle_connected(self):
-        logging.info("Started processing")
-        conn.plugin['xep_0045'].joinMUC(room,
-            'creep',
-            wait=True)
-        logging.info("Connected to chat room '%s'" % room)
-        app.run(host=host, port=port)
+conn.process(threaded=True)
+def handle_connected(self):
+    logging.info("Started processing")
+    conn.plugin['xep_0045'].joinMUC(room,
+        'creep',
+        wait=True)
+    logging.info("Connected to chat room '%s'" % room)
+    app.run(host=host, port=port)
 
-    conn.add_event_handler("session_start", handle_connected)
+conn.add_event_handler("session_start", handle_connected)
