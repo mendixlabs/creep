8480c6cbd937df6708857ac198e3f9f41dbc399e
Achiel van der Mandele <achiel@kretek.local>
2012-05-26 21:52:40 +0200

    working initial version:  - log everything as debug to console  - settings in config file: http & xmpp sections  - send to recipient specified by config  - take POST data, if application/json, parse message, otherwise pipe    all payload

--- bot.py
+++ bot.py
@@ -1,11 +1,35 @@
 from flask import Flask, request, render_template, redirect
+from ConfigParser import ConfigParser
+import sleekxmpp
+import logging
 
+logging.basicConfig(level=logging.DEBUG)
+
+config = ConfigParser()
+config.read(['creep.cfg', '/usr/local/etc/creep.cfg'])
 app = Flask(__name__)
-@app.route("/", methods=['POST',])
-def index():
-    print dir(request)
-    print request.json
-    return request.json
 
 if __name__ == "__main__":
-    app.run()
+    jid = config.get('xmpp', 'username')
+    password = config.get('xmpp', 'password')
+    to = config.get('xmpp', 'recipient')
+    server = config.get('xmpp', 'server')
+    port = int(config.get('http', 'port'))
+
+    conn = sleekxmpp.ClientXMPP('%s/collector' % jid, password)
+
+    @app.route("/", methods=['POST',])
+    def index():
+        if request.content_type == 'application/json':
+            msg = request.json['message']
+        else:
+            msg = request.data
+        conn.send_message(mto=to, mbody="%s" % msg)
+        return "message sent"
+
+    print("Connecting %s to xmppserver %s" % (jid, server))
+    conn.connect((server, 5222))
+    print("Connected")
+
+    conn.process(threaded=True)
+    app.run(port=port)
