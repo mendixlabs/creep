aff1a01527ef4d51c6bb75bff19410d1eceba715
Achiel van der Mandele <git@elsuxor.net>
2012-06-10 18:17:09 +0200

    creep-client:  - conference room is configurable  - authentication can be configured in either /usr/local/etc/creep.cfg    or local creep.cfg. Omit auth header if no cfg is found.

--- examples/usr/local/bin/creep.py
+++ examples/usr/local/bin/creep.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 import sys
 import json
+import yaml
+import os
+
+yamlfile = '/usr/local/etc/creep.yaml' if os.path.isfile('/usr/local/etc/creep.yaml') else 'creep.yaml'
+config = yaml.load(open(yamlfile))
 
 text = "\n".join(iter(sys.stdin.readline, ""))
-body = {"message": text, "room": "room@conference.example.com"}
+body = {"message": text, "room": config['xmpp']['default_room']}
 
 print json.dumps(body)
--- examples/usr/local/bin/creep_json.sh
+++ examples/usr/local/bin/creep_json.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-curl -s -S -m 2 --data @- -H "Content-Type:application/json" -H "Creep-Authentication:base64string==" https://creep.example.com
+if [ -f "/usr/local/etc/creep.cfg" ]
+then
+    source /usr/local/etc/creep.cfg
+    curl -s -S -m 2 --data @- -H "Content-Type:application/json" -H "Creep-Authentication:$secret" https://creep.example.com
+elif [ -f "creep.cfg" ]
+then
+    source creep.cfg
+    curl -s -S -m 2 --data @- -H "Content-Type:application/json" -H "Creep-Authentication:$secret" https://creep.example.com
+else
+    curl -s -S -m 2 --data @- -H "Content-Type:application/json"  https://creep.example.com
+fi
+
