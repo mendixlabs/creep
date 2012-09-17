66d479ef18d62673e06c86ed21ba2aca2bea86e6
Achiel van der Mandele <git@elsuxor.net>
2012-09-17 21:12:17 +0200

    refactor plugin loading

--- plugins/plugin.py
+++ plugins/plugin.py
@@ -6,21 +6,28 @@ class Plugin():
     provides = []
     pass
 
-def load_plugins(activated_plugins, xmpp, config):
+def load_plugins(names, xmpp, config):
     handlers = {}
-    for activated_plugin in activated_plugins:
-        plugin = __import__('plugins.%s' % activated_plugin, fromlist=['plugins',])
-        for i in dir(plugin):
-            item = getattr(plugin, i)
-            if inspect.isclass(item) and issubclass(item, Plugin) and not item == Plugin:
-                plugin_instance = item(xmpp, config=config)
-                for p in plugin_instance.provides:
-                    if p in handlers:
-                        raise Exception("Handler already defined for command '%s'" % p)
-                    if not hasattr(plugin_instance, p):
-                        raise Exception("Plugin '%s' doesn't provide '%s'" 
-                                % (plugin_instance, p))
-                    handlers[p] = getattr(plugin_instance, p)
-                    logging.info("registered '%s' as a handler for '%s'" % 
-                            (plugin_instance, p))
+    for name in names:
+        load_plugin(name, handlers, xmpp, config)
     return handlers
+
+def load_plugin(name, handlers, xmpp, config):
+    plugin = __import__('plugins.%s' % name, fromlist=['plugins',])
+    for attribute in dir(plugin):
+        item = getattr(plugin, attribute)
+        if inspect.isclass(item) and issubclass(item, Plugin) and not item == Plugin:
+            plugin_instance = item(xmpp, config=config)
+            for handler_name in plugin_instance.provides:
+                register_handler(handler_name, plugin_instance, handlers)
+
+def register_handler(handler_name, plugin_instance, handlers):
+    if handler_name in handlers:
+        raise Exception("Handler already defined for command '%s'" % p)
+    if not hasattr(plugin_instance, handler_name):
+        raise Exception("Plugin '%s' doesn't provide '%s'" 
+                % (plugin_instance, handler_name))
+    handlers[handler_name] = getattr(plugin_instance, handler_name)
+    logging.info("Registered '%s' as a handler for '%s'" % 
+            (plugin_instance, handler_name))
+
