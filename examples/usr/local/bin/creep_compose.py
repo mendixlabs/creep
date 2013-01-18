efbf673f5afa711c906d9d5c4ad4349ad32cb5a4
Xiwen Cheng <xiwen.cheng@mendix.com>
2013-01-18 15:51:09 +0100

    improve the creep json client with support for multi-line messages

---
+++ examples/usr/local/bin/creep_compose.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import sys
+import json
+import os
+
+# room must be passed as the first argument
+if len(sys.argv) > 1:
+    room = sys.argv[1]
+else:
+    sys.stderr.write("Usage: echo fooo | %s room@conference.example.com" % sys.argv[0])
+    exit(1)
+
+text = "".join(iter(sys.stdin.readline, ""))
+body = {"message": text, "room": room}
+
+print json.dumps(body)
--- examples/usr/local/bin/creep_json.sh
+++ examples/usr/local/bin/creep_json.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-if [ -f "/usr/local/etc/creep.cfg" ]
-then
-    source /usr/local/etc/creep.cfg
-    curl -s -S -m 2 --data @- -H "Content-Type:application/json" -H "Creep-Authentication:$secret" https://creep.example.com
-elif [ -f "creep.cfg" ]
-then
-    source creep.cfg
-    curl -s -S -m 2 --data @- -H "Content-Type:application/json" -H "Creep-Authentication:$secret" https://creep.example.com
-else
-    curl -s -S -m 2 --data @- -H "Content-Type:application/json"  https://creep.example.com
-fi
+# Never edit this file!
+
+# priority: low to high
+configs="/etc/creep.cfg /usr/local/etc/creep.cfg creep.cfg"
+
+# override the following variables in .cfg file
+url="https://creep.example.com"
+secret="floep"
+
+for i in $configs; do
+    [ -f "$i" ] && config="$i"
+done
+
+# load config if found
+[ -f "$config" ] && source "$config"
+
+curl -s -S -m 2 --data @- -H "Content-Type:application/json" -H "Creep-Authentication:$secret" $url
 
