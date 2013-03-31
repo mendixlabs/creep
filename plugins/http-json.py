78bfeeff2ee75e229925842ef0bc75d917d108bd
Achiel van der Mandele <git@elsuxor.net>
2013-04-01 00:17:56 +0200

    rewrite httpserver to BaseHTTPServer, as this gives us finegrained control over threading.
    Plugin listens for poison: 'keep_running', after which it terminates.
    shutdown hook poisons thread, after which it fires a 'dummie final'
    request to allow thread to die gracefully.

--- plugins/http-json.py
+++ plugins/http-json.py
@@ -1,41 +1,87 @@
-from flask import Flask, request, make_response, render_template, redirect
+from threading import Thread
+import xmlrpclib
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import cgi
 from base64 import decodestring
 from plugin import Plugin
-from multiprocessing import Process
 
-app = Flask(__name__)
 
+'''
+TODO
+ - move config to instance variable (static bad)
+ - neater way of firing 'last' request
+'''
 class HttpJson(Plugin):
 
     def __init__(self, xmpp, config=None):
         kwargs = {'host' : config['http']['host'], 'port' : config['http']['port']}
+        self.host = config['http']['host']
+        self.port = config['http']['port']
         HttpJson.config = config
         HttpJson.xmpp = xmpp
-        self.server = Process(target=app.run)
-        self.server.start()
+        server_address = ('', 8000)
+        self.httpd = HTTPServer(server_address, Handler)
+
+        self.thread = Thread(target=self.run)
+        self.thread.start()
+
         print "initialized http-json"
 
-    @app.route("/", methods=['POST',])
-    def index():
+    def run(self):
+        self.keep_running = True
+        while self.keep_running:
+            self.httpd.handle_request()
+
+    def shutdown(self):
+        self.keep_running = False
+        self.fire_dummy_request()
+
+    def fire_dummy_request(self):
+        try:
+            server = xmlrpclib.Server('http://%s:%s' % 
+                                      (self.host, self.port))
+            server.ping()
+        except:
+            pass
+
+class Handler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+
         if 'secret' in HttpJson.config['http']:
-            if 'Creep-Authentication' in request.headers:
+            if 'Creep-Authentication' in self.headers:
                 provided_secret = decodestring(request.headers['Creep-Authentication']).rstrip()
                 if isinstance(HttpJson.config['http']['secret'], list):
                     if not provided_secret in HttpJson.config['http']['secret']:
-                        return make_response("forbidden", 403)
+                        self.return_forbidden()
+                        return
                 elif provided_secret != HttpJson.config['http']['secret']:
-                    return make_response("forbidden", 403)
+                    self.return_forbidden()
+                    return
             else:
-                return make_response("forbidden", 403)
+                self.return_forbidden()
+                return
 
-        if request.content_type == 'application/json':
+        length = int(self.headers['content-length'])
+        request_content = self.rfile.read(length)
+
+        if ctype == 'application/json':
             msg = request.json['message']
             room = request.json['room']
         else:
-            msg = request.data
+            msg = request_content
             room = HttpJson.config['xmpp']['default_room']
-        HttpJson.xmpp.send_message(mto=room, mbody="%s" % msg, mtype='groupchat')
-        return ""
 
-    def shutdown(self):
-        self.server.terminate()
+        HttpJson.xmpp.send_message(mto=room, mbody="%s" % msg, mtype='groupchat')
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write('ok')
+        self.wfile.close()
+
+    def return_forbidden(self):
+        self.send_response(403)
+        self.end_headers()
+        self.wfile.write('forbidden')
+        self.wfile.close()
+
