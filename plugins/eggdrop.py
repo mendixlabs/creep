9b98270eecbd37b8e369268d40408d4fc09f1f2d
Achiel van der Mandele <git@elsuxor.net>
2014-01-20 23:56:23 +0100

    only allow admins to kick users

--- plugins/eggdrop.py
+++ plugins/eggdrop.py
@@ -10,6 +10,10 @@ class EggDrop(Plugin):
         creep.xmpp.registerPlugin('xep_0045')
         self.muc = creep.xmpp.plugin['xep_0045']
         self.xmpp = creep.xmpp
+        if config is not None and 'admins' in config:
+            self.admins = config['admins']
+        else:
+            self.admins = []
 
     def kick(self, message=None, origin=None):
         '''Kick users
@@ -21,10 +25,19 @@ class EggDrop(Plugin):
             nick = message
             reason = 'Connection reset by peer'
 
-        room = str(origin).split('/')[0]
+        (room, requester_nick) = str(origin).split('/')
         if nick not in self.muc.getRoster(room):
             return "%s is not in this room" % nick
 
+        requester = self.muc.getJidProperty(room, str(requester_nick), 'jid')
+        origin_bare = str(requester).split('/')[0]
+        if origin_bare not in self.admins:
+            return "You're not an admin"
+ 
+        self._send_kick_iq(nick, reason, room)
+        return "kicked %s" % nick
+
+    def _send_kick_iq(self, nick, reason, room):
         query = ET.Element('{http://jabber.org/protocol/muc#admin}query')
         item = ET.Element('item', {'role':'none', 'nick':nick})    
         reason_element = ET.Element('reason')    
@@ -34,7 +47,6 @@ class EggDrop(Plugin):
         query.append(item)
         iq['to'] = room
         result = iq.send()
-        return "kicked %s" % nick
 
     def __str__(self):
         return 'eggdrop'
