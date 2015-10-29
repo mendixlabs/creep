c3211c73f2f69f2fa49085461224fff08a52db6e
Rik de Groot <rik.de.groot@mendix.com>
2015-10-29 20:30:47 +0100

    Add proper logging.

--- slack.py
+++ slack.py
@@ -1,12 +1,14 @@
 import time
 import json
+import logging
 from slackclient import SlackClient
 from threading import Thread
 from random import random
 
 class Slack():
 
-  def __init__(self, creep, config, debug=True):
+  def __init__(self, creep, config):
+    logging.basicConfig(level=logging.INFO)
     self.debug = debug
     self.creep = creep
     self.token = config["slack"]["token"]
@@ -23,6 +25,7 @@ class Slack():
   def start(self):  
     self.thread = Thread(target=self._run)
     self.thread.start()
+    logging.debug("Started slack service integration")
   
   def _run(self):
     self.keep_running = True
@@ -38,19 +41,21 @@ class Slack():
       if channel:
         return channel[0]["id"]
       else:
-        raise Exception("Channel '" + self.channel + "' does not exists yet!")
-    self._debug("Not connected.")
+        logging.exception("Channel '" + self.channel + "' does not exists yet!")
+    else:
+      logging.info("Not connected.")
     return None
   
   def get_user_id(self, config):
      if self.connected:
        user_list = json.loads(self.client.api_call("users.list"))
        user = filter(lambda u: u["profile"]["email"] == config["slack"]["email"], user_list["members"])
-     if user:
-        return user[0]["id"]
+       if user:
+         return user[0]["id"]
+       else:
+         logging.exception("Unable to locate user: " + config["slack"]["email"])
       else:
-        raise Exception("Unable to locate user: " + config["slack"]["email"])
-    self._debug("Not connected.")
+        logging.info("Not connected.")
     return None
   
   def connect(self, config):
@@ -59,7 +64,7 @@ class Slack():
       self.get_channel_id()
       self.client.rtm_send_message(self.channel, lines[int(random()*len(lines))])
     else:
-      print "Connection Failed, invalid token?"
+      logging.exception("Connection Failed, invalid token: " + config["slack"]["token"])
       self.connected = False
     return None
   
@@ -68,24 +73,22 @@ class Slack():
       if not channel:
         channel = self.channel
       self.client.rtm_send_message(channel, message)
-    return None
-    
-  def _debug(self, message=None):
-    if self.debug:
-      print message
+      logging.debug("%s| %s" % channel, message)
+    else:  
+      logging.info("Not connected")
     return None
     
   def message_read(self, message):
     m = message[0]
-    
     if 'type' in m.keys() and m["type"] == "message" and 'text' in m.keys() and m["text"]:
       if m["text"].startswith("<@" +self.user_id + ">"):
         self.send_message(self._highlight())
-      if m["channel"][0]=="D": # direct message
+      elif m["channel"][0]=="D": # direct message
         channel = m["channel"]
-          
         response = self.creep.handle_message(m["text"], self)
         self.send_message(response, channel)
+    else:
+      logging.debug("message ignored:| %s" % message)
     return None
   
   def _highlight(self):
