be20113f67177712afd100c5f8453dee4f6dae84
Jouke Waleson <jouke.waleson@mendix.com>
2016-03-23 09:57:19 +0100

    restart every 6 hours to work around the crash bug

--- __main__.py
+++ __main__.py
@@ -19,8 +19,14 @@ if __name__ == '__main__':
 
     def handle_ctrl_c(signal, frame):
         creep.shutdown()
+        sys.exit(0)
+
+    def shutdown(signum, frame):
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, handle_ctrl_c)
+    signal.signal(signal.SIGALRM, shutdown)
+    signal.alarm(3600 * 6)
     # wait for a signal, so the main thread does not vanish (which means it
     # would not be there anymore to react on ctrl-c)
     try:
